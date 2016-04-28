bce3742dcf3d9be18e5bda9c2f447960d8c7e859
Darren Klein <darren.a.klein@gmail.com>
2016-04-27 20:16:23 -0400

    Commit success! Got the basics down for adding a popup after each node added to the map.

--- carto_doodle_text.js
+++ carto_doodle_text.js
@@ -19,104 +19,42 @@ $(document).ready(function(){
     
 
     
+    
+    var popUpFields = "What's here?</br><input type='text' />"
+    
+    
+    
+    
+    
 
     map.on('draw:created', function(e){
-        
         featureGroup.addLayer(e.layer);
-        
-        //geoObject = JSON.stringify(e.layer.toGeoJSON());
-        
-        //geoObject_array.push(geoObject);
-        
-        //geoObject_index = geoObject_array.length - 1;
-        
-        //$('#doodle_form').append('<input name="geoObject['+geoObject_index+']" value='+geoObject+' />')
-        
+        e.layer.bindPopup(popUpFields).openPopup();
+    });
+    
 
-        
-        
-        
-        /*
-        $('#layer_type').val(e.layerType);
-        
-        if(e.layerType == 'polygon' || e.layerType == 'rectangle'){
-            
-            polyGroup = "";
 
-            for(i = 0; i < e.layer._latlngs.length; i++){
-                lng = e.layer._latlngs[i].lng;
-                lat = e.layer._latlngs[i].lat;
-                
-                if(i == e.layer._latlngs.length - 1){
-                    //THIS IS NECESSARY FOR A POLYGON - POLYGONS WILL ALWAYS HAVE AN EXTRA SET OF COORDINATES AT THE END WHICH ARE IDENTIAL TO THE FIRST SET OF COORDINATES - CLOSES IT UP, OTHERWISE IT WOULD BE A LINE...
-                    lngLatGroup = lng + ' ' + lat + ', ' + e.layer._latlngs[0].lng + ' ' + e.layer._latlngs[0].lat;
-                }
-                else{
-                    lngLatGroup = lng + ' ' + lat + ', ';
-                };
-                
-                polyGroup += lngLatGroup;
-            };
-            
-            $('#polygon').val(polyGroup);
-        }
-        else if(e.layerType == 'polyline'){
-            
-            lineGroup = "";
+    
+    geoObjectString_array = [];
+    type_array = [];
 
-            for(i = 0; i < e.layer._latlngs.length; i++){
-                lng = e.layer._latlngs[i].lng;
-                lat = e.layer._latlngs[i].lat;
-                
-                if(i == e.layer._latlngs.length - 1){
-                    lngLatGroup = lng + ' ' + lat;
-                }
-                else{
-                    lngLatGroup = lng + ' ' + lat + ', ';
-                };
-                
-                lineGroup += lngLatGroup;
-            };
-            
-            $('#line').val(lineGroup);  
-        }
-        else if(e.layerType == 'circle'){
-            console.log(e.layer.toGeoJSON());
-            $('#latitude').val(e.layer._latlng.lat);
-            $('#longitude').val(e.layer._latlng.lng); 
-            $('#radius').val(e.layer._mRadius);
-        }
-        else{
-            $('#latitude').val(e.layer._latlng.lat);
-            $('#longitude').val(e.layer._latlng.lng);
+
+    $("#doodle_form").submit(function(){
+
+        $.each(featureGroup._layers, function(key, value){
+            geoObject = value.toGeoJSON();
+            type = geoObject.geometry.type;
+            geoObjectString = JSON.stringify(value.toGeoJSON());
+            geoObjectString_array.push(geoObjectString);
+            type_array.push(type);
+        });
+
+        for(i = 0; i < geoObjectString_array.length; i++){
+            $('#doodle_form').append('<input type="text" name="geoObject['+i+']" value='+geoObjectString_array[i]+' />');
+            $('#doodle_form').append('<input type="text" name="type['+i+']" value='+type_array[i]+' />');
         };
-        */
 
     });
-
-    
-
-    
-        geoObjectString_array = [];
-        type_array = [];
-    
-    
-        $("#doodle_form").submit(function(){
-            
-            $.each(featureGroup._layers, function(key, value){
-                geoObject = value.toGeoJSON();
-                type = geoObject.geometry.type;
-                geoObjectString = JSON.stringify(value.toGeoJSON());
-                geoObjectString_array.push(geoObjectString);
-                type_array.push(type);
-            });
-            
-            for(i = 0; i < geoObjectString_array.length; i++){
-                $('#doodle_form').append('<input type="text" name="geoObject['+i+']" value='+geoObjectString_array[i]+' />');
-                $('#doodle_form').append('<input type="text" name="type['+i+']" value='+type_array[i]+' />');
-            };
-            
-        });
     
     
 });
